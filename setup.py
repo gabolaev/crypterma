678ac588f6d0a09bc2913c332aef679759c3c569
Nikola Trubitsyn <nikola.trubitsyn@gmail.com>
2018-10-09 19:04:59 +0300

    Split a long line

--- setup.py
+++ setup.py
@@ -9,9 +9,10 @@ setup(
     license='MIT',
     python_requires='>=3.6',
     description='ASCII-chart version of how Bitcoin grows',
-    long_description='''A console application that allows you to see a graph of the Bitcoin price change in any time interval. Also prints the current cost and comparison with yesterday's value.
-Almost all currencies are supported.
-More info: https://github.com/gabolaev/crypterma/blob/master/README.md''',
+    long_description='''A console application that allows you to see a graph of the Bitcoin price change in any time interval.
+                        Also prints the current cost and comparison with yesterday's value.
+                        Almost all currencies are supported.
+                        More info: https://github.com/gabolaev/crypterma/blob/master/README.md''',
     packages=['crypterma', 'crypterma.configs'],
     install_requires=[
         'termcolor',
